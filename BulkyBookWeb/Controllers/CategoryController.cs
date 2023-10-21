f1b072fdd31c97882d0b7534ac12ee16c2c58f17
Orhun Aka <orhunaka@outlook.com>
2023-10-21 15:03:08 +0300

    finished the project

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -19,10 +19,97 @@ namespace BulkyBookWeb.Controllers
             return View(objCategoryList);
         }
         
-        //This will be a get action method.
+        // This will be a get action method.
         public IActionResult Create()
         {
             return View();
+        }
+
+        // This will be a post action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category created successfully.";
+                return RedirectToAction("Index"); // If another controller, define on the right side.
+            }
+            return View(obj);
+            
+        }
+
+        // This will be a get action method.
+        public IActionResult Edit( int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        // This will be a post action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category updated successfully.";
+                return RedirectToAction("Index"); // If another controller, define on the right side.
+            }
+            return View(obj);
+
+        }
+
+        // This will be a get action method.
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        // This will be a post action method
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+                
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["Success"] = "Category deleted successfully.";
+            return RedirectToAction("Index");
         }
     }
 }
